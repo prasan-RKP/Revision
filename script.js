701b8c1339e4d38fd9664b03a8c9a64d5540f628
Prasan RKP <prasan543e@gmail.com>
2026-02-09 20:29:42 +0530

    reviesd question with a string  concept

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Q.1) Even/Odd Numbers
 
-let a = 0;
+let a = 45;
 if (a === 0) {
-  console.log("It a zero Number");
+  console.log("The number is ZERO");
 } else if (a % 2 === 0) {
-  console.log("It is Even Number");
+  console.log("The number is Even");
 } else {
-  console.log("It is a odd number");
+  console.log("The number is odd");
 }
 
 // Q.2) Reverse a String without using .reverse()
 
-let str = "solo";
+let str = "facebook";
 let reverse = "";
 
-for (let i = str.split("").length; i > 0; i--) {
-  reverse += str[i - 1];
+for (let i = 1; i <= str.split("").length; i++) {
+  reverse += str[str.split("").length - i];
 }
 
 console.log(reverse);
@@ -33,8 +33,31 @@ for (let i = 1; i < arr.length; i++) {
 
 console.log(large);
 
-
 // Q.4) Remove duplicates from array
 
-let dupArr = ['a', 'e', 'e', 'i' ,'o', 'u', 'u'];
-console.log([...new Set(dupArr)]);
+let dupArr = ["a", "e", "e", "i", "o", "u", "u"];
+let tempArr = [];
+
+for (let i = 0; i < dupArr.length; i++) {
+  if (!tempArr.includes(dupArr[i])) {
+    tempArr.push(dupArr[i]);
+  }
+}
+
+console.log("solved", tempArr);
+
+// Q.5) Write a function that counts vowels in a string.
+
+let myStr = "ducatin";
+let vowel = ["a", "e", "i", "o", "u"];
+let count = 0;
+const countVowels = () => {
+  for (let i = 0; i < myStr.split("").length; i++) {
+    if (vowel.includes(myStr[i])) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(countVowels());
